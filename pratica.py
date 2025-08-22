8644c9b286cb52a85aaf0f335906c992ded466e2
GUILHERME BRASIL PEREIRA <pereira.1958@aluno.pr.senac.br>
2025-08-22 16:56:39 -0300

    SQL

--- pratica.py
+++ pratica.py
@@ -11,22 +11,27 @@ conexao = pymysql.connect(
 )
 
 #LISTA TODOS OS ALUNOS COM NOME, EMAIL, SEMESTRE E STATUS
-with conexao.cursor() as cursor:
-    cursor.execute("SELECT * FROM alunos")
-    alunos = cursor.fetchall() # RETORNA TODOS OS ALUNOS
+try:
+    with conexao.cursor() as cursor:
+        cursor.execute("SELECT * FROM alunos")
+        alunos = cursor.fetchall() # RETORNA TODOS OS ALUNOS
 
     # MOSTRA OS DADOS
-    for aluno in alunos:
-        print(f"Aluno: {aluno['nome']} - Email: {aluno['email']} - Semestre: {aluno['semestre_atual']} - Status: {aluno['status_aluno']}")
+        for aluno in alunos:
+            print(f"Aluno: {aluno['nome']} - Email: {aluno['email']} - Semestre: {aluno['semestre_atual']} - Status: {aluno['status_aluno']}")
 
 # PERMITE CADASTRAR UM NOVO ALUNO COM DADOS DIGITADOS PELO USUÁRIO
-aluno = input("Digite o nome do aluno: ")
-email = input("Digite o email do aluno: ")
-semestre = input("Digite o semestre do aluno: ")
-status = input("Digite o status do aluno: ")
+    aluno = input("Digite o nome do aluno: ")
+    email = input("Digite o email do aluno: ")
+    semestre = input("Digite o semestre do aluno: ")
+    status = input("Digite o status do aluno: ")
 
-with conexao.cursor() as cursor:
-    sql = "INSERT INTO alunos (nome, email, semestre_atual, status_aluno) VALUES (%s, %s, %s, %s)"
-    valores = (aluno, email, semestre, status)
-    cursor.execute(sql, valores)
-    conexao.commit() # SALVANDO BANCO
+    with conexao.cursor() as cursor:
+        sql = "INSERT INTO alunos (nome, email, semestre_atual, status_aluno) VALUES (%s, %s, %s, %s)"
+        valores = (aluno, email, semestre, status)
+        cursor.execute(sql, valores)
+        conexao.commit() # SALVANDO BANCO
+except Exception as e:
+    print(f"Erro ao cadastrar aluno: {e}")
+finally:
+    conexao.close()
